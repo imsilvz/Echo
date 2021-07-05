62c840e9b0ba5e7724f2a36dbf5b00ab0178486d
Richard Yawger <rjyawger@me.com>
2021-07-05 02:53:26 -0400

    job color highlighting

--- WebApp/src/Content/Common/ChatLink.jsx
+++ WebApp/src/Content/Common/ChatLink.jsx
@@ -3,6 +3,211 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Typography from '@material-ui/core/Typography';
 
+const PlayerJobs = {}
+function AddJobInfo(dataLst) {
+    for(let i=0; i<dataLst.length; i++) {
+        let data = dataLst[i];
+        let JobInfo = {}
+        if(data.BaseClass) {
+            Object.assign(
+                JobInfo, 
+                PlayerJobs[data.BaseClass]
+            );
+        }
+        Object.assign(JobInfo, data);
+        PlayerJobs[JobInfo.Acronym] = JobInfo;
+    }
+}
+AddJobInfo([
+    {
+        Acronym: "Unknown",
+        Name: "Unknown",
+    },
+    {
+        Acronym: "GLD",
+        Name: "Gladiator",
+        Color: "#7FDBFF"
+    },
+    {
+        Acronym: "PGL",
+        Name: "Pugilist",
+        Color: "#d5c400",
+    },
+    {
+        Acronym: "MRD",
+        Name: "Marauder",
+        Color: "#FF4136",
+    },
+    {
+        Acronym: "LNC",
+        Name: "Lancer",
+        Color: "#33A0FF"
+    },
+    {
+        Acronym: "ARC",
+        Name: "Archer",
+        Color: "#01FF70",
+    },
+    {
+        Acronym: "CNJ",
+        Name: "Conjurer",
+        Color: "#FFFFE8",
+    },
+    {
+        Acronym: "THM",
+        Name: "Thaumaturge",
+        Color: "#E365F6",
+    },
+
+    // Disciples of the Hand
+    {
+        Acronym: "CPT",
+        Name: "Carpenter",
+    },
+    {
+        Acronym: "BSM",
+        Name: "Blacksmith",
+    },
+    {
+        Acronym: "ARM",
+        Name: "Armorer",
+    },
+    {
+        Acronym: "GSM",
+        Name: "Goldsmith"
+    },
+    {
+        Acronym: "LTW",
+        Name: "Leatherworker",
+    },
+    {
+        Acronym: "WVR",
+        Name: "Weaver",
+    },
+    {
+        Acronym: "ALC",
+        Name: "Alchemist",
+    },
+    {
+        Acronym: "CUL",
+        Name: "Culinarian",
+    },
+
+    // Disciples of the Land
+    {
+        Acronym: "MIN",
+        Name: "Miner",
+    },
+    {
+        Acronym: "BTN",
+        Name: "Botanist",
+    },
+    {
+        Acronym: "FSH",
+        Name: "Fisher",
+    },
+
+    // Disciples of War / Magic
+    {
+        Acronym: "PLD",
+        BaseJob: "GLD",
+        Name: "Paladin",
+    },
+    {
+        Acroynm: "MNK",
+        BaseJob: "PGL",
+        Name: "Monk",
+    },
+    {
+        Acronym: "WAR",
+        BaseJob: "MRD",
+        Name: "Warrior",
+    },
+    {
+        Acronym: "DRG",
+        BaseJob: "LNC",
+        Name: "Dragoon",
+    },
+    {
+        Acronym: "BRD",
+        BaseJob: "ARC",
+        Name: "Bard",
+    },
+    {
+        Acronym: "WHM",
+        BaseJob: "CNJ",
+        Name: "White Mage",
+    },
+    {
+        Acronym: "BLM",
+        BaseJob: "THM",
+        Name: "Black Mage",
+    },
+    {
+        Acronym: "ACN",
+        Name: "Arcanist",
+    },
+    {
+        Acronym: "SMN",
+        Name: "Summoner",
+        Color: "#2ECC40",
+    },
+    {
+        Acronym: "SCH",
+        Name: "Scholar",
+        Color: "#bba9cd",
+    },
+    {
+        Acronym: "ROG",
+        Name: "Rogue",
+        Color: "#A796AB",
+    },
+    {
+        Acronym: "NIN",
+        BaseJob: "ROG",
+        Name: "Ninja",
+    },
+    {
+        Acronym: "MCH",
+        Name: "Machinist",
+        Color: "#DBF7FF",
+    },
+    {
+        Acronym: "DRK",
+        Name: "Dark Knight",
+        Color: "#f79414",
+    },
+    {
+        Acronym: "AST",
+        Name: "Astrologian",
+        Color: "#edd25f",
+    },
+    {
+        Acronym: "SAM",
+        Name: "Samurai",
+        Color: "#b8b3b1",
+    },
+    {
+        Acronym: "RDM",
+        Name: "Red Mage",
+        Color: "#EB9EBA"
+    },
+    {
+        Acronym: "BLU",
+        Name: "Blue Mage",
+    },
+    {
+        Acronym: "GNB",
+        Name: "Gunbreaker",
+        Color: "#f0b885",
+    },
+    {
+        Acronym: "DNC",
+        Name: "Dancer",
+        Color: "#f8d3c9"
+    }
+]);
+
 function useHover(ref) {
     const [val, setVal] = React.useState(false);
 
@@ -39,6 +244,15 @@ const ChatLink = (props) => {
     const style = {
         cursor: isHover ? "pointer" : "auto",
         backgroundColor: isHover ? color : "transparent"
+    }
+
+    if(isPlayer) {
+        let job = `${isPlayer.Job}`.padStart(2, '0');
+        let struct = PlayerJobs[job];
+        if(struct.Color) {
+            style.color = struct.Color;
+            style.backgroundColor = struct.backgroundColor;
+        }
     }
 
     const handleClick = (event) => {
