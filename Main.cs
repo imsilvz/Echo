90c3a85b9bfe6ed7f2a6225c7d27596bd3eabc39
Richard Yawger <rjyawger@me.com>
2021-07-08 14:21:22 -0400

    check for webview install and throw an error if the runtime is not found

--- Main.cs
+++ Main.cs
@@ -1,7 +1,10 @@
 using System;
 using System.IO;
+using System.Windows;
+using System.Diagnostics;
 
 using Echo.Core.Controllers;
+using Microsoft.Web.WebView2.Core;
 namespace Echo 
 {
     public class Program 
@@ -9,13 +12,25 @@ namespace Echo
         [STAThread]
         public static void Main(string[] args)
         {
-            // Create Directory!
-            var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            Directory.CreateDirectory($"{path}/Echo/cache");
+            try
+            {
+                var version = CoreWebView2Environment.GetAvailableBrowserVersionString();
+                // Do something with `version` if needed.
 
-            DataBroker.Instance.Initialize();
-            var app = new App();
-            app.Run();
+                // Create Directory!
+                var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                Directory.CreateDirectory($"{path}/Echo/cache");
+
+                DataBroker.Instance.Initialize();
+                var app = new App();
+                app.Run();
+            }
+            catch (WebView2RuntimeNotFoundException exception)
+            {
+                // Handle the runtime not being installed.
+                // `exception.Message` is very nicely specific: It (currently at least) says "Couldn't find a compatible Webview2 Runtime installation to host WebViews."
+                MessageBox.Show("WebView2 is not installed!", "Echo - FFXIV Chat Listener", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
