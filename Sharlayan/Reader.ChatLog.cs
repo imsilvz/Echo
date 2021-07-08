0bddc1b43034782ff403a68a25878176b4ee856b
Richard Yawger <rjyawger@me.com>
2021-07-07 22:03:46 -0400

    fixed first message being cut off

--- Sharlayan/Reader.ChatLog.cs
+++ Sharlayan/Reader.ChatLog.cs
@@ -63,10 +63,12 @@ namespace Sharlayan {
                 long currentArrayIndex = (this._chatLogReader.ChatLogPointers.OffsetArrayPos - this._chatLogReader.ChatLogPointers.OffsetArrayStart) / 4;
                 if (this._chatLogReader.ChatLogFirstRun)
                 {
+                    // Commented these so that the whole chatlog is loaded.
                     this._chatLogReader.EnsureArrayIndexes();
                     this._chatLogReader.ChatLogFirstRun = false;
-                    this._chatLogReader.PreviousOffset = this._chatLogReader.Indexes[0];//this._chatLogReader.Indexes[(int) currentArrayIndex - 1];
+                    this._chatLogReader.PreviousOffset = 0;//this._chatLogReader.Indexes[0];//this._chatLogReader.Indexes[(int) currentArrayIndex - 1];
                     this._chatLogReader.PreviousArrayIndex = 0;//(int) currentArrayIndex - 1;
+                    Debug.WriteLine(this._chatLogReader.Indexes[0]);
                 }
                 else {
                     if (currentArrayIndex < this._chatLogReader.PreviousArrayIndex) {
